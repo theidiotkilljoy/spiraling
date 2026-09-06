25608b2376ab2f3595c45c8f8326f59d68135c75
theidiotkilljoy <theidiotkilljoy@gmail.com>
2026-09-05 23:33:37 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // Paste your Firebase web app configuration here.
 // Firebase configuration values are identifiers, not private server secrets.
 export const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME.firebaseapp.com",
-  databaseURL: "https://REPLACE_ME-default-rtdb.firebaseio.com",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME.firebasestorage.app",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME"
+  apiKey: "AIzaSyAmigmfs27UegRxIfMLkP4g-0uDtfcNzbc",
+  authDomain: "spiraling-c2df8.firebaseapp.com",
+  projectId: "spiraling-c2df8",
+  storageBucket: "spiraling-c2df8.firebasestorage.app",
+  messagingSenderId: "590487321291",
+  appId: "1:590487321291:web:235c22309d51a7e971b94e",
+  measurementId: "G-KJVMEFB296"
 };
 
 // Used until the DM sets a room-specific difficulty.
